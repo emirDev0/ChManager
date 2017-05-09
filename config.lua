622656ffb0cc0894439799bd4e30ee9f90721ded
emirDev0 <emirDev.vmp@gmail.com>
2017-05-09 09:29:08 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "363486531:AAFAvMV0AzuMoE9eutSdSPLxyXcBiYGiiOw"
+bot_token = "299026041:AAGV4EznBKMqp77hwJ1FD4_0tm1tfQcARhI"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
 sudo_id = 212556314
